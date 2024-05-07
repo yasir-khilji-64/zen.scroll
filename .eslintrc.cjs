e9191f40f3598398fbe0d0a65af99ff432c28c51
Yasir Hassan <yasir.hassan1993@hotmail.com>
2024-05-07 13:29:59 +0500

    chore: Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  extends: [
+    'next/core-web-vitals',
+    'airbnb-base',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+};
